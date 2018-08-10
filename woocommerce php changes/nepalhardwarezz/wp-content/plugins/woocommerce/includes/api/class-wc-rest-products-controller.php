5421731c4748b762154b13470bbf9ed4f67993c2
PK <pradhyu.shrestha@gmail.com>
2018-08-09 22:18:32 -0400

    commented unwanted attributes

--- woocommerce php changes/nepalhardwarezz/wp-content/plugins/woocommerce/includes/api/class-wc-rest-products-controller.php
+++ woocommerce php changes/nepalhardwarezz/wp-content/plugins/woocommerce/includes/api/class-wc-rest-products-controller.php
@@ -166,7 +166,7 @@ class WC_REST_Products_Controller extends WC_REST_Legacy_Products_Controller {
 		$data     = $this->add_additional_fields_to_object( $data, $request );
 		$data     = $this->filter_response_by_context( $data, $context );
 		$response = rest_ensure_response( $data );
-		$response->add_links( $this->prepare_links( $object, $request ) );
+/* 		$response->add_links( $this->prepare_links( $object, $request ) ); */
 
 		/**
 		 * Filter the data for a response.
@@ -388,14 +388,14 @@ class WC_REST_Products_Controller extends WC_REST_Legacy_Products_Controller {
 
 			$images[] = array(
 				'id'                => (int) $attachment_id,
-				'date_created'      => wc_rest_prepare_date_response( $attachment_post->post_date, false ),
+/* 				'date_created'      => wc_rest_prepare_date_response( $attachment_post->post_date, false ),
 				'date_created_gmt'  => wc_rest_prepare_date_response( strtotime( $attachment_post->post_date_gmt ) ),
 				'date_modified'     => wc_rest_prepare_date_response( $attachment_post->post_modified, false ),
-				'date_modified_gmt' => wc_rest_prepare_date_response( strtotime( $attachment_post->post_modified_gmt ) ),
+				'date_modified_gmt' => wc_rest_prepare_date_response( strtotime( $attachment_post->post_modified_gmt ) ), */
 				'src'               => current( $attachment ),
 				'name'              => get_the_title( $attachment_id ),
-				'alt'               => get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ),
-				'position'          => (int) $position,
+/* 				'alt'               => get_post_meta( $attachment_id, '_wp_attachment_image_alt', true ),
+				'position'          => (int) $position, */
 			);
 		}
 
@@ -403,14 +403,14 @@ class WC_REST_Products_Controller extends WC_REST_Legacy_Products_Controller {
 		if ( empty( $images ) ) {
 			$images[] = array(
 				'id'                => 0,
-				'date_created'      => wc_rest_prepare_date_response( current_time( 'mysql' ), false ), // Default to now.
+/* 				'date_created'      => wc_rest_prepare_date_response( current_time( 'mysql' ), false ), // Default to now.
 				'date_created_gmt'  => wc_rest_prepare_date_response( current_time( 'timestamp', true ) ), // Default to now.
 				'date_modified'     => wc_rest_prepare_date_response( current_time( 'mysql' ), false ),
-				'date_modified_gmt' => wc_rest_prepare_date_response( current_time( 'timestamp', true ) ),
+				'date_modified_gmt' => wc_rest_prepare_date_response( current_time( 'timestamp', true ) ), */
 				'src'               => wc_placeholder_img_src(),
 				'name'              => __( 'Placeholder', 'woocommerce' ),
-				'alt'               => __( 'Placeholder', 'woocommerce' ),
-				'position'          => 0,
+/* 				'alt'               => __( 'Placeholder', 'woocommerce' ),
+				'position'          => 0, */
 			);
 		}
 
